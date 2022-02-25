107982cdf36b43c35a85361accfdf0697bfa6738
joelryt <rytkonenjoel@gmail.com>
2022-02-25 13:02:13 +0200

    Add subscription to face_location_topic

---
+++ src/eye_movement/eye_movement/eye_movement.py
@@ -0,0 +1,48 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from builtin_interfaces.msg import Duration
+from face_tracker_msgs.msg import Point2
+
+
+class EyeMoverClient(Node):
+
+    def __init__(self):
+        super().__init__('eye_mover_client')
+        self._action_client = ActionClient(self, FollowJointTrajectory, '/eyes_controller/follow_joint_trajectory')
+        self.subscription = self.create_subscription(Point2, 'face_location_topic', self.listener_callback, 10)
+
+    def listener_callback(self, msg):
+        self.get_logger().info('x: %d, y: %d' % (msg.x, msg.y))
+
+    def send_goal(self, vertical, horizontal):
+        goal_msg = FollowJointTrajectory.Goal()
+        trajectory_points = JointTrajectoryPoint(positions=[vertical, horizontal], time_from_start=Duration(sec=1, nanosec=0))
+        goal_msg.trajectory = JointTrajectory(joint_names=['eyes_shift_vertical_joint', 'eyes_shift_horizontal_joint'],
+                                              points=[trajectory_points])
+
+        self._send_goal_future = self._action_client.wait_for_server()
+
+        self._action_client.send_goal_async(goal_msg)
+
+
+def main():
+    print('Hi from eye_movement.')
+
+    rclpy.init()
+
+    action_client = EyeMoverClient()
+
+    action_client.send_goal(-0.3, -0.4)
+
+    rclpy.spin(action_client)
+
+    # Shutdown
+    action_client.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
