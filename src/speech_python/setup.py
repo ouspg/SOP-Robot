ad6263733b85a54ba96576185c307c5ef9dff34d
vagrant <vagrant@vagrant-ros>
2023-02-13 00:00:55 +0000

    Yksinkertaiset publisher ja subscriber funktiot tehty, äänen tallennus mikrofonista onnistuu Googlen apilla, kustomoitujen viestien rakentaminen aloitettu

---
+++ src/speech_python/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'speech_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Juha-Matti Runtti',
+    maintainer_email='jruntti20@student.oulu.fi',
+    description='Publisher/Subscriber speech recognition module with rclpy',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = speech_python.publisher_member_function:main',
+            'listener = speech_python.subscriber_member_function:main'
+            'recognizer = speech_python.speech_rec:main'
+        ],
+    },
+)
