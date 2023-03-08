6ee58f92c5b2ff2a33efdc951a4daaf1c5eec479
Markus <mk473203@outlook.com>
2023-03-08 17:49:09 +0200

    Added basic jaw movement node

---
+++ src/jaw_movement/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'jaw_movement'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vagrant',
+    maintainer_email='vagrant@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'jaw_movement_node = jaw_movement.jaw_movement_node:main'
+        ],
+    },
+)
