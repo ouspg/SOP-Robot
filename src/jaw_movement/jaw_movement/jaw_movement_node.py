0b61006d5e3dbda7256f56b8007b8223359617f6
Ville Nättinen <villenattinen@gmail.com>
2023-04-19 21:55:13 +0300

    jaw_movement_node: Added comments for TODOs. Implemented handling of spaces in input string

--- src/jaw_movement/jaw_movement/jaw_movement_node.py
+++ src/jaw_movement/jaw_movement/jaw_movement_node.py
@@ -12,6 +12,10 @@ from face_tracker_msgs.msg import Point2
 
 class JawMoverNode(Node):
 
+    """
+    TODO: 
+    Get input from speech synthesis
+    """
     def __init__(self):
         super().__init__('jaw_mover_client')
         self._action_client = ActionClient(self, FollowJointTrajectory, '/jaw_controller/follow_joint_trajectory')
@@ -21,30 +25,40 @@ class JawMoverNode(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
         self.i = 0
-        self.input = 'Tällä vain testataan suun liikettä'
+        self.jawPos = 0.0
+        self.charDuration = Duration(sec=0, nanosec=0)
+
+        self.input = 'Tällä vain testataan suun liikettä ppppp pppppp ooooooooo '
         
         self.get_logger().info('Jaw mover client initialized.')
         
         
+    """
+    TODO: 
+    1. Handle each input string only once instead of looping -> recognize end of string
+    2. Get real duration of characters (into variable charDuration)
+    3. Handle spaces in string (position, duration)
+    """
     def timer_callback(self):
 
         goal_msg = FollowJointTrajectory.Goal()
-        #jawPos = 0.1 + 0.4 * (self.i % 2)
         self.i = self.i % len(self.input) 
-
         char = self.input[self.i].lower()
-        jawPos = self.synch_jaw_to_speech(char)
-        trajectory_points = JointTrajectoryPoint(positions=[jawPos], time_from_start=Duration(sec=0, nanosec=0))
+        if char != ' ':
+            self.synch_jaw_to_speech(char)
+        trajectory_points = JointTrajectoryPoint(positions=[self.jawPos], time_from_start=self.charDuration)
         goal_msg.trajectory = JointTrajectory(joint_names=['head_jaw_joint'], points=[trajectory_points])
         self._send_goal_future = self._action_client.wait_for_server()
-        self.get_logger().info('Jaw: ' + str(jawPos) + ' Letter: ' + char)
+        self.get_logger().info('Jaw: ' + str(self.jawPos) + ' Letter: ' + char)
         self._action_client.send_goal_async(goal_msg)
         self.i += 1
-
+    
+    """
+    TODO: 
+    Define and return real duration for each character in speech
+    """
     def synch_jaw_to_speech(self, char):
-        """
-        TODO: Match duration to each letter in speech
-        """
+
         vowels = ['a','e','i','o','u','y','ä','ö']
         rounded = ['o','u','y','ö']
         bilabial = ['m','p','b']
@@ -52,18 +66,24 @@ class JawMoverNode(Node):
         coronal = ['t','d','n','s','r','l']
         if char in vowels:
             if char in rounded:
-                return 0.35
+                self.jawPos = 0.35
+                self.charDuration = Duration(sec=0, nanosec=0)
             else:
-                return 0.5
+                self.jawPos = 0.5
+                self.charDuration = Duration(sec=0, nanosec=0)
         else:
             if char in bilabial:
-                return 0.0 
+                self.jawPos = 0.0
+                self.charDuration = Duration(sec=0, nanosec=0)
             elif char in labiodental:
-                return 0.1
+                self.jawPos = 0.1
+                self.charDuration = Duration(sec=0, nanosec=0)
             elif char in coronal:
-                return 0.2
+                self.jawPos = 0.2
+                self.charDuration = Duration(sec=0, nanosec=0)
             else:
-                return 0.3
+                self.jawPos = 0.3
+                self.charDuration = Duration(sec=0, nanosec=0)
 
 def main():
     print('Hello from jaw_movement.')
