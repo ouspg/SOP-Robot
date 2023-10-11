81edef28e4a25c91dc33aac356ac8edf24e6538f
laurilako <konsta.laurila1@gmail.com>
2023-10-11 13:13:56 +0300

    Start to create better dataset for answers. Add greeting functionality to chatbot

--- src/qabot/qabot/qabot.py
+++ src/qabot/qabot/qabot.py
@@ -7,7 +7,7 @@ import rclpy
 from std_msgs.msg import String
 from tts_msgs.srv import StringToWav
 
-import logging
+#import logging
 
 from haystack.document_stores import InMemoryDocumentStore
 import os
@@ -25,7 +25,10 @@ class QaBotClientNode(Node):
 
     def __init__(self):
         super().__init__('qabot_node')
-        self.publisher_ = self.create_publisher(String, 'chatbot_response', 10)
+        self.publisher_ = self.create_publisher(
+            String, 
+            'chatbot_response', 
+            10)
         self.subscription = self.create_subscription(
             String, 
             'recognized_speech', 
@@ -33,27 +36,34 @@ class QaBotClientNode(Node):
             10)
         self.get_logger().info('Starting!')
         self.document_store = InMemoryDocumentStore(use_bm25=True)
-        self.doc_dir = "./src/qabot/resource/"
+        self.doc_dir = "./src/qabot/resource/data/"
         self.files_to_index = [self.doc_dir + "/" + f for f in os.listdir(self.doc_dir)]
         self.indexing_pipeline = TextIndexingPipeline(self.document_store)
         self.indexing_pipeline.run_batch(file_paths=self.files_to_index)
         self.retriever = BM25Retriever(document_store=self.document_store)
         self.reader = FARMReader(model_name_or_path="TurkuNLP/bert-base-finnish-cased-squad2", use_gpu=False)
         self.pipe = ExtractiveQAPipeline(self.reader, self.retriever)
+        self.greetings = ["terve", "hei", "päivää", "moi", "iltaa", "huomenta", "moikka"]
 
     def tts_callback(self, msg):
-        data = self.chatbot_worker_callback(msg.data)
-        if (data != "Anteeksi, en ymmärtänyt."):
+        data = self.chatbot_worker_callback((msg.data).lower())
+        if(data):
             tts_msg = String()
             tts_msg.data = data
             self.publisher_.publish(tts_msg)
-            
+        else:
+            pass
+        
     def chatbot_worker_callback(self, data):
+        if(data in self.greetings):
+            return("terve")
+        else:
             final_response_text = "Anteeksi, en ymmärtänyt."
             pred = self.pipe.run(
-                query=data, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+                query=data, params={"Retriever": {"top_k": 7}, "Reader": {"top_k": 3}}
             )
             final_response_text = pred['answers'][0].answer
+            self.get_logger().info("Found answer: %s" % (final_response_text))
             return(final_response_text)
 
 def main():
