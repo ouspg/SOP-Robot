a71dc0a1fb6d3574b280cf540792fe8541b8d522
Veikko Vähäsöyrinki <veikko.vahasoyrinki@protonmail.ch>
2023-10-20 14:22:33 +0300

    Added unified arms client

--- client/unified_arms_client.py
+++ client/unified_arms_client.py
@@ -1,143 +1,23 @@
 import rclpy
 import time
 import random
+import serial
 from std_msgs.msg import String
 from rclpy.node import Node
 from trajectory_msgs.msg import JointTrajectoryPoint
 from trajectory_msgs.msg import JointTrajectory
+from control_msgs.action import FollowJointTrajectory
+from builtin_interfaces.msg import Duration
 
+# Define the Arduino serial port and baud rate
+SERIAL_PORT = '/dev/ttyACM0'  # Replace with the appropriate port
+BAUD_RATE = 115200
 
-class RightHandActionClient(Node):
-    # TODO unify Left and Right HandAction client
-    # by defining left/right when instancing the class
-    def __init__(self):
-        super().__init__("right_hand_action_client")
-        self.right_hand_gesture_publisher = self.create_publisher(String, "/r_hand/r_hand_topic", 10)
-        self.available_commands  = [
-            "open",
-            "fist",
-            "scissors",
-            "point",
-            "thumbs_up",
-            "grasp",
-            "pen_grasp",
-            "hard_rock",
-            "rps",
-            "funny"
-        ]
-        self.exit_commands = ["quit", "exit"]
-        self.logger = self.get_logger()
+# Define the ROS2 topic names
+TOPIC_NAME = 'shoulder_controller/joint_trajectory'
+FEEDBACK_TOPIC_NAME = 'feedback'
 
-
-    def send_gesture(self, gesture):
-        msg = String()
-        msg.data = gesture
-        self.right_hand_gesture_publisher.publish(msg)
-
-
-    def list_available_commands(self):
-        print("Available commands:", end=" ")
-        for command in self.available_commands[:-1]:
-            print(command, end=", ")
-        print(self.available_commands[-1])
-        print("You can also input 'quit' or 'exit' to quit.")
-
-
-    def rps(self):
-        self.logger.info(f"Starting rps")
-        self.send_gesture("three")
-        time.sleep(2)
-        self.send_gesture("scissors")
-        time.sleep(2)
-        self.send_gesture("point")
-        time.sleep(2)
-        self.send_gesture(random.choice(["open", "fist", "scissors"]))
-
-
-    def main(self):
-        command = ""
-        while command not in self.exit_commands:
-            command = input("Input command: ").lower()
-            if command in self.available_commands:
-                if command == "rps":
-                    self.rps()
-                else:
-                    self.send_gesture(command)
-            elif command not in self.exit_commands:
-                self.list_available_commands()
-
-
-class LeftHandActionClient(Node):
-    def __init__(self):
-        super().__init__("left_hand_action_client")
-        self.left_hand_gesture_publisher = self.create_publisher(String, "/l_hand/l_hand_topic", 10)
-        self.available_commands  = [
-            "open",
-            "fist",
-            "scissors",
-            "point",
-            "thumbs_up",
-            "grasp",
-            "pen_grasp",
-            "hard_rock",
-            "rps",
-            "funny"
-        ]
-        self.exit_commands = ["quit", "exit"]
-        self.logger = self.get_logger()
-
-
-    def send_gesture(self, gesture):
-        msg = String()
-        msg.data = gesture
-        self.left_hand_gesture_publisher.publish(msg)
-
-
-    def list_available_commands(self):
-        print("Available commands:", end=" ")
-        for command in self.available_commands[:-1]:
-            print(command, end=", ")
-        print(self.available_commands[-1])
-        print("You can also input 'quit' or 'exit' to quit.")
-
-
-    def rps(self):
-        self.logger.info(f"Starting rps")
-        self.send_gesture("three")
-        time.sleep(2)
-        self.send_gesture("scissors")
-        time.sleep(2)
-        self.send_gesture("point")
-        time.sleep(2)
-        self.send_gesture(random.choice(["open", "fist", "scissors"]))
-
-
-    def main(self):
-        command = ""
-        while command not in self.exit_commands:
-            command = input("Input command: ").lower()
-            if command in self.available_commands:
-                if command == "rps":
-                    self.rps()
-                else:
-                    self.send_gesture(command)
-            elif command not in self.exit_commands:
-                self.list_available_commands()
-
-# Arms (Shoulders):
-
-class ShoulderController(Node):
-    def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(JointTrajectory, 'shoulder_controller/joint_trajectory', 10)
-        self.args = ['trial', 'zero', 'rps_1']
-        self.positions_dict = {
-            "zero": [30.0, 90.0, 10.0, 0.0, 34.0, 80.0, 10.0, 0.0],
-            "rps_1": [75.0, 90.0, 80.0, 0.0, 34.0, 80.0, 10.0, 0.0]
-        }
-        self.logger = self.get_logger()
-
-
+"""
     def main(self):
         arg = input("Input command: ").lower()
         while arg != "quit":
@@ -149,31 +29,116 @@ class ShoulderController(Node):
                 self.send_action(arg)
             time.sleep(2)
             arg = input("Input command: ").lower()
+"""
 
 
+
+class UnifiedArms(Node):
+    def __init__(self):
+        #ShoulderController
+        super().__init__('minimal_publisher')
+
+        # Initialize the serial connection
+        self.serial = serial.Serial(SERIAL_PORT, BAUD_RATE)
+        # Create a ROS2 publisher for the feedback
+        self.publisher                    = self.create_publisher(String, FEEDBACK_TOPIC_NAME, 10)
+        self.left_hand_gesture_publisher  = self.create_publisher(String, "/l_hand/l_hand_topic", 10)
+        self.right_hand_gesture_publisher = self.create_publisher(String, "/r_hand/r_hand_topic", 10)
+        
+        #Create main program subscriber
+        self.gesture_subscription = self.create_subscription(String, "/arms/arm_action", self.action_callback, 10)
+        
+        #self.publisher_ = self.create_publisher(JointTrajectory, 'shoulder_controller/joint_trajectory', 10)
+        self.args = ['wave', 'rock']
+        self.positions_dict = {
+            "zero": [30.0, 90.0, 10.0, 0.0, 34.0, 80.0, 10.0, 0.0],
+            "rps_1": [30.0, 90.0, 10.0, 0.0, 79.0, 80.0, 45.0, 0.0],
+            "rps_2": [30.0, 90.0, 10.0, 0.0, 79.0, 80.0, 75.0, 0.0]
+        }
+        
+        self.available_commands  = [
+            "open",
+            "fist",
+            "scissors",
+            "point",
+            "thumbs_up",
+            "grasp",
+            "pen_grasp",
+            "hard_rock",
+            "rps",
+            "funny"
+        ]
+
+        self.exit_commands = ["quit", "exit"]
+        self.logger = self.get_logger()
+
+
+    def action_callback(self, msg):
+        arg = msg.data
+        hand = "r"
+        self.logger.info(f"arg: {arg}")
+        if arg not in self.args:
+            self.logger.info("Action not implemented")
+        else:
+            if arg in ['wave']:
+                self.action_wave()
+            if arg in ['rock']:
+                self.action_rock()
+
+    def action_wave(self):
+        self.l_hand_gesture("fist")
+        time.sleep(0.5)
+        self.send_action("rps_1")
+        time.sleep(0.5)
+        self.send_action("rps_2")
+        time.sleep(0.5)
+        self.send_action("rps_1")
+        time.sleep(0.5)
+        self.send_action("rps_2")
+        time.sleep(0.5)
+        self.send_action("rps_1")
+        time.sleep(0.5)
+        self.send_action("zero")
+        time.sleep(0.5)
+        self.l_hand_gesture("open")
+
+
+    def action_rock(self):
+        self.l_hand_gesture("hard_rock")
+        time.sleep(1)
+        self.send_action("rps_1")
+        time.sleep(1)
+        self.send_action("rps_2")
+        time.sleep(1)
+        self.send_action("rps_1")
+        time.sleep(1)
+        self.send_action("rps_2")
+        time.sleep(1)
+        self.send_action("rps_1")
+        time.sleep(1)
+        self.send_action("rps_2")
+        time.sleep(1)
+        self.send_action("zero")
+        time.sleep(1)
+        self.l_hand_gesture("open")
+
+
+
+
+#ShoulderController
     def available_commands(self):
         print("Command not recognized, available commands are")
         print(f"{self.args[:-1]}")
         print(f"and quit.")
 
-
     def send_action(self, action):
         self.logger.info(f"Action: {action}")
-        self.send_goal(self.positions_dict[action])
-
+        self.arm_gesture(self.positions_dict[action])
 
     def trial(self):
         command = []
         i = 0
-        joints = [
-            "R_shoulder lift",
-            "R_upper arm roll",
-            "R_bicep",
-            "R_shoulder out","L_shoulder lift",
-            "L_upper arm roll",
-            "L_bicep",
-            "L_shoulder out"
-        ]
+        joints = ["R_shoulder lift", "R_upper arm roll", "R_bicep", "R_shoulder out","L_shoulder lift", "L_upper arm roll", "L_bicep", "L_shoulder out"]
         while len(command) < 8:
             angle = float(input(f"Angle for {joints[i]} joint: "))
             if isinstance(angle, float) and angle >= 0 and angle <= 180:
@@ -182,21 +147,12 @@ class ShoulderController(Node):
             else:
                 print("Angle must be int. Safe values are 0 - 180")
         self.logger.info("Sending positions")
-        self.send_goal(command)
+        self.arm_gesture(command)
 
-
-    def send_goal(self, action):
+    def arm_gesture(self, action):
         goal_msg = JointTrajectory()
-        goal_msg.joint_names = [
-            "r_shoulder_lift_joint",
-            "r_shoulder_out_joint",
-            "r_upper_arm_roll_joint",
-            "r_elbow_flex_joint",
-            "l_shoulder_lift_joint",
-            "l_shoulder_out_joint",
-            "l_upper_arm_roll_joint",
-            "l_elbow_flex_joint"
-        ]
+        goal_msg.joint_names = ["r_shoulder_lift_joint", "r_shoulder_out_joint", "r_upper_arm_roll_joint", "r_elbow_flex_joint",
+        "l_shoulder_lift_joint", "l_shoulder_out_joint", "l_upper_arm_roll_joint", "l_elbow_flex_joint"]
         point = JointTrajectoryPoint()
         point.positions = action
         dur = Duration()
@@ -206,39 +162,67 @@ class ShoulderController(Node):
 
         self.logger.info(f"Sending request")
         print(type(goal_msg))
-        self.publisher_.publish(goal_msg)
+        #self.publisher_.publish(goal_msg)
+
+        # Extract the desired angles from the received message
+        angles = []
+        for point in goal_msg.points:
+            angles.extend(point.positions)
+
+        # Prepare the command to be sent to the Arduino
+        command = ','.join(str(angle) for angle in angles)
+
+        # Send the command to the Arduino
+        self.serial.write(command.encode())
+
+        # Read the feedback from the Arduino
+        feedback = self.serial.readline().decode().strip()
+        self.get_logger().info('Received feedback: %s' % feedback)
+
+        # Publish the feedback to the ROS2 topic
+        feedback_msg = String()
+        feedback_msg.data = feedback
+        self.publisher.publish(feedback_msg)
+
+   
+#Hands
+    def l_hand_gesture(self, gesture):
+        msg = String()
+        msg.data = gesture
+        self.left_hand_gesture_publisher.publish(msg)
+
+    def r_hand_gesture(self, gesture):
+        msg = String()
+        msg.data = gesture
+        self.right_hand_gesture_publisher.publish(msg)
+
+
+
+    def list_available_commands(self):
+        print("Available commands:", end=" ")
+        for command in self.available_commands[:-1]:
+            print(command, end=", ")
+        print(self.available_commands[-1])
+        print("You can also input 'quit' or 'exit' to quit.")
+
+
+    def rps(self):
+        self.logger.info(f"Starting rps")
+        self.send_gesture("three")
+        time.sleep(2)
+        self.send_gesture("scissors")
+        time.sleep(2)
+        self.send_gesture("point")
+        time.sleep(2)
+        self.send_gesture(random.choice(["open", "fist", "scissors"]))
+ 
 
 
 def main(args=None):
     rclpy.init(args=args)
-
-    action_clients = {}
-    action_clients["right_hand"] = RightHandActionClient()
-    action_clients["left_hand"] = LeftHandActionClient()
-    action_clients["shoulders"] = ShoulderController()
-
-    exit_commands = ["exit", "quit"]
-    client_selection = ""
-
-    while client_selection not in exit_commands:
-        client_selection = input("Select body part to actuate: ").lower()
-        if client_selection in action_clients:
-            active_client = action_clients[client_selection]
-            command = ""
-            while command not in exit_commands:
-                command = input("Input command: ").lower()
-                if command in active_client.available_commands:
-                    if command == "rps":
-                        active_client.rps()
-                    else:
-                        active_client.send_gesture(command)
-                elif command not in exit_commands:
-                    active_client.list_available_commands()
-        elif client_selection not in exit_commands:
-            for part in action_clients:
-                print(part)
-
-    rclpy.shutdown()
+    armController = UnifiedArms()
+    rclpy.spin(armController)
+    
 
 
 if __name__ == "__main__":
