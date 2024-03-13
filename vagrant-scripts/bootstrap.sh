75465129c7b6e204dd11055ff9e68f0fd7dc6fa4
Felix Schurk <felix.schurk@rwth-aachen.de>
2024-03-13 09:02:22 +0200

    remove commented out code from bootstrap.sh

--- vagrant-scripts/bootstrap.sh
+++ vagrant-scripts/bootstrap.sh
@@ -19,23 +19,10 @@ apt update
 # setup GNOME desktop
 apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
-# Install ROS1
-# sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-# apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-# apt-get update
-# apt-get install -y ros-noetic-desktop-full ros-noetic-moveit python3-catkin-tools python3-catkin-lint python3-rosdep python3-roslaunch ros-noetic-moveit-visual-tools ros-noetic-dynamixel-sdk
-# apt-get install -y gitg vim meld terminator
-# rosdep init
-# rosdep fix-permissions
-# sudo -u vagrant rosdep update
-
 # Install ROS2
 apt install curl gnupg2 lsb-release software-properties-common
 curl -s 'https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc' | apt-key add -
 sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-
-# add-apt-repository ppa:deadsnakes/ppa
-#   python3.7 \
 
 apt update
 
@@ -96,24 +83,10 @@ apt install -y \
 sudo timedatectl set-timezone "Europe/Helsinki"
 sudo systemctl enable --now systemd-timesyncd.service
 
-# Install ROS2 Foxy from tarball
-# curl -L -o /tmp/ros2-foxy.tar.bz2 https://github.com/ros2/ros2/releases/download/release-foxy-20201211/ros2-foxy-20201211-linux-focal-amd64.tar.bz2
-# mkdir -p /ros2_foxy
-# cd /ros2_foxy
-# tar -xf /tmp/ros2-foxy.tar.bz2 -C /ros2_foxy
-# rm /tmp/ros2-foxy.tar.bz2
-# chown -R vagrant /ros2_foxy/
-# sudo -u vagrant rosdep update
-# sudo -u vagrant rosdep install --from-paths ros2-linux/share --ignore-src --rosdistro foxy -y --skip-keys "console_bridge fastcdr fastrtps osrf_testing_tools_cpp poco_vendor rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp rti-connext-dds-5.3.1 tinyxml_vendor tinyxml2_vendor urdfdom urdfdom_headers"
-
 # Setup Dynamixel U2D2 (https://emanual.robotis.com/docs/en/software/dynamixel/dynamixel_workbench/#u2d2)
 wget https://raw.githubusercontent.com/ROBOTIS-GIT/dynamixel-workbench/master/99-dynamixel-workbench-cdc.rules -P /etc/udev/rules.d/
 udevadm control --reload-rules
 udevadm trigger
-
-# Change python3 symlink from python3.8 to python3.7 (ROS2 does not work with python 3.8 currently)
-# rm /usr/bin/python3
-# ln -s /usr/bin/python3.7 /usr/bin/python3
 
 # Allow access to shared folders
 adduser vagrant vboxsf
