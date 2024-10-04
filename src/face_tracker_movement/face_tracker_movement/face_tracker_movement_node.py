83713c0ee0d074909102fd7bbb0fe610283effda
Matti Luukkonen <matti.luukkonen12@gmail.com>
2024-10-04 17:44:25 +0300

    Move head gestures
    Move head gestures from head_gestures_node to face_tracker_movement_node

--- src/face_tracker_movement/face_tracker_movement/face_tracker_movement_node.py
+++ src/face_tracker_movement/face_tracker_movement/face_tracker_movement_node.py
@@ -12,7 +12,7 @@ from control_msgs.msg import JointTrajectoryControllerState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from builtin_interfaces.msg import Duration
 from face_tracker_msgs.msg import Point2, Faces
-from std_msgs.msg import Float32
+from std_msgs.msg import String, Float32
 
 
 class FaceTrackerMovementNode(Node):
@@ -30,7 +30,30 @@ class FaceTrackerMovementNode(Node):
         self.face_list_subscription = self.create_subscription(Faces, '/face_tracker/faces', self.face_list_callback, 2)
         self.head_state_subscription = self.create_subscription(JointTrajectoryControllerState, '/head_controller/controller_state', self.head_state_callback, 5)
         self.eyes_state_subscription = self.create_subscription(JointTrajectoryControllerState, '/eyes_controller/controller_state', self.eyes_state_callback, 5)
-        self.head_gesture_length_subscription = self.create_subscription(Float32, '/head_gestures/length', self.head_gesture_callback, 1)
+
+        self.head_gesture_subscription = self.create_subscription(String, '/head_gestures/head_gesture_topic', self.head_gesture_callback, 10)
+
+        """
+        # Values that should work with the actual hardware
+        self.head_vertical_lower_limit = 0.8
+        self.head_vertical_upper_limit = 1.5
+        self.head_pan_lower_limit = -0.25
+        self.head_pan_upper_limit = 1.75
+        self.eye_vertical_lower_limit = -0.7
+        self.eye_vertical_upper_limit = -0.2
+        self.eye_horizontal_lower_limit = -2.0
+        self.eye_horizontal_upper_limit = 0.0
+        """
+
+        # Values that should work in the simulator
+        self.head_vertical_lower_limit = -0.3
+        self.head_vertical_upper_limit = 0.3
+        self.head_pan_lower_limit = -2.0
+        self.head_pan_upper_limit = 2.0
+        self.eye_vertical_lower_limit = -0.5
+        self.eye_vertical_upper_limit = 0.5
+        self.eye_horizontal_lower_limit = -0.6
+        self.eye_horizontal_upper_limit = 0.6
 
         # Middle point of image view
         self.middle_x = 640
@@ -108,14 +131,26 @@ class FaceTrackerMovementNode(Node):
             else:
                 self.eyes_state[i] = val
 
-    # When doing a head gesture, pause face tracking for the duration in order not to override the gesture
     def head_gesture_callback(self, msg):
-        temp = (self.eyes_enabled, self.head_enabled)
-        self.eyes_enabled = False
-        self.head_enabled = False
-        time.sleep(msg.data)
-        self.eyes_enabled = temp[0]
-        self.head_enabled = temp[1]
+        gesture = msg.data
+        self.logger.info(gesture)
+        gesture = gesture.split(",")
+        args = {}
+        for i in range(1, len(gesture)):
+            gesture[i] = gesture[i].strip(" ")
+            if gesture[i][:10] == "magnitude=":
+                args['magnitude'] = float(gesture[i][10:])
+            if gesture[i][:6] == "delay=":
+                args['delay'] = float(gesture[i][6:])
+            if gesture[i][:9] == "duration=":
+                args['duration_of_individual_movements'] = float(gesture[i][9:])
+        gesture = gesture[0]
+        if gesture == 'nod':
+            self.nod(**args)
+        elif gesture == 'shake':
+            self.head_shake(**args)
+        else:
+            self.logger.info("Gesture not implemented")
 
     # Get random horizontal positions for eyes and head and turn there at a random speed
     def idle_timer_callback(self):
@@ -173,6 +208,108 @@ class FaceTrackerMovementNode(Node):
                 time.sleep(0.3)
         
         time.sleep(0.2)
+
+    def nod(self, magnitude=0.4, delay=0.5, duration_of_individual_movements=0.4):
+        """
+        Makes the head nod.
+        Takes two optional arguments:
+            magnitude: The interval between the highest and lowest points of the nod
+            delay: The delay (in seconds) between the start of each individual movement
+            duration_of_individual_movements: The time (in seconds) it takes for an individual movement to finish. Should be less than delay.
+        """
+        if self.head_state:
+            verticalTilt_start = self.head_state[3]
+            msg = Float32()
+            msg.data = delay * 3
+            # self.head_gesture_length_publisher.publish(msg)
+
+            self.fixed_gaze_head_turn('up', magnitude / 2, duration_of_individual_movements)
+            time.sleep(delay)
+            self.fixed_gaze_head_turn('down', magnitude, duration_of_individual_movements)
+            time.sleep(delay)
+            verticalTilt = self.head_state[3]
+            self.fixed_gaze_head_turn('up', verticalTilt - verticalTilt_start, duration_of_individual_movements)
+
+    def head_shake(self, magnitude=0.5, delay=0.5, duration_of_individual_movements=0.4):
+        """
+        Shakes the head.
+        Takes two optional arguments:
+            magnitude: The interval between the leftmost and rightmost points of the head shakes
+            delay: The delay (in seconds) between the start of each individual movement
+            duration_of_individual_movements: The time (in seconds) it takes for an individual movement to finish. Should be less than delay.
+        """
+        self.logger.info("head_shake")
+        self.logger.info(f"head_shake: {magnitude =}")
+        self.logger.info(f"head_shake: {delay =}")
+        self.logger.info(f"head_shake: {duration_of_individual_movements =}")
+        self.logger.info(f"head_shake: {self.head_state =}")
+        self.logger.info(f"head_shake: {self.eyes_state =}")
+        if self.head_state:
+            self.logger.info("head state: True")
+            pan_start = self.head_state[0]
+            msg = Float32()
+            msg.data = delay * 3
+            # self.head_gesture_length_publisher.publish(msg)
+            self.fixed_gaze_head_turn('left', magnitude / 2, duration_of_individual_movements)
+            time.sleep(delay)
+            self.fixed_gaze_head_turn('right', magnitude, duration_of_individual_movements)
+            time.sleep(delay)
+            pan = self.head_state[0]
+            self.fixed_gaze_head_turn('left', pan_start - pan, duration_of_individual_movements)
+
+    def fixed_gaze_head_turn(self, direction, magnitude, duration=0.4):
+        """
+        Turns the head while keeping the gaze steady by rotating the eyes in the opposite direction as the head.
+        Decreases the magnitude of the movement, if it would exceed the maximum range of either the head or the eyes. 
+        Takes 3 arguments: direction, magnitude and duration.
+            direction: The direction of the head turn. Can be "left", "right", "up" or "down".
+            magnitude: The magnitude of the head turn.
+            duration: The time in seconds it should take to finish the head turn.
+        """
+        self.logger.info(f"fixed_gaze_head_turn: {direction =}")
+        self.logger.info(f"fixed_gaze_head_turn: {magnitude =}")
+        self.logger.info(f"fixed_gaze_head_turn: {duration =}")
+        duration = Duration(sec=0, nanosec=int(duration * 100000000))
+        pan, _, _, verticalTilt = self.head_state
+        eye_x, eye_y = self.eyes_state
+
+        if direction == 'left':
+            if pan + magnitude > self.head_pan_upper_limit:
+                magnitude = self.head_pan_upper_limit - pan
+            if eye_x - magnitude < self.eye_horizontal_lower_limit:
+                magnitude = self.eye_horizontal_lower_limit - eye_x
+            self.logger.info(f"fixed_gaze_head_turn: in left:{magnitude =}")
+
+            self.send_pan_and_vertical_tilt_goal(pan + magnitude, verticalTilt, duration)
+            self.send_eye_goal(eye_x - magnitude, eye_y, duration)
+        
+        elif direction == 'right':
+            if pan - magnitude < self.head_pan_lower_limit:
+                magnitude = pan - self.head_pan_lower_limit
+            if eye_x + magnitude > self.eye_horizontal_upper_limit:
+                magnitude = self.eye_horizontal_upper_limit - eye_x
+            self.logger.info(f"fixed_gaze_head_turn: in right:{magnitude =}")
+
+            self.send_pan_and_vertical_tilt_goal(pan - magnitude, verticalTilt, duration)
+            self.send_eye_goal(eye_x + magnitude, eye_y, duration)
+        
+        elif direction == 'up':
+            if verticalTilt - magnitude < self.head_vertical_lower_limit:
+                magnitude = verticalTilt - self.head_vertical_lower_limit
+            if eye_y + magnitude > self.eye_vertical_upper_limit:
+                magnitude = self.eye_vertical_upper_limit - eye_y
+
+            self.send_pan_and_vertical_tilt_goal(pan, verticalTilt - magnitude, duration)
+            self.send_eye_goal(eye_x, eye_y + magnitude)
+        
+        elif direction == 'down':
+            if verticalTilt + magnitude > self.head_vertical_upper_limit:
+                magnitude = self.head_vertical_upper_limit - verticalTilt
+            if eye_y - magnitude < self.eye_vertical_lower_limit:
+                magnitude = eye_y - self.eye_vertical_lower_limit
+
+            self.send_pan_and_vertical_tilt_goal(pan, verticalTilt + magnitude, duration)
+            self.send_eye_goal(eye_x, eye_y - magnitude)
 
     def send_eye_goal(self, horizontal, vertical, duration=None):
         # The eyes lock up if they try to move too fast so it'll go a bit slower for longer movements (also faster for short movements)
