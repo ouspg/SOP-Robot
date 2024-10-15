bdfba4ac569c7075d6516885db2f6ae97344fc05
Matias Paavilainen <101323404+matiaspaavilainen@users.noreply.github.com>
2024-10-15 13:35:02 +0300

    updated test to use AUnit

--- Arduino/testSplitting/testSplitting.ino
+++ Arduino/testSplitting/testSplitting.ino
@@ -1,76 +1,96 @@
+#include <AUnit.h>
+
 const int NUM_SERVOS = 9;
 
+String extractServos(String command) {
+  // Split command string by commas and colons
+  int servosToMove[NUM_SERVOS];
+  int angles[NUM_SERVOS];
+  int angleIndex = 0;
 
-int check(int value, int min, int max){
-  return (value + min < max) ? (min + value) : ((min + value > max ) ? max : value);
+  int start = 0;
+  int split = command.indexOf(':') + 1;
+  int end = command.indexOf(',');
+
+  while (end != -1) {
+    // Assign ids and angles to arrays
+    servosToMove[angleIndex] = command.substring(start, split).toInt();
+    angles[angleIndex] = command.substring(split, end).toInt();
+    angleIndex++;
+
+    start = end + 1;
+    split = command.indexOf(':', start) + 1;
+    end = command.indexOf(',', start);
+  }
+  
+  // get the last pair
+  servosToMove[angleIndex] = command.substring(start, split).toInt();
+  angles[angleIndex] = command.substring(split).toInt();
+  angleIndex++;
+
+  // Send back the received angles
+  String string1 = "Received angles: ";
+
+  for (int i = 0; i < angleIndex; ++i) {
+    string1.concat(angles[i]);
+    if (i < angleIndex - 1) {
+      string1.concat(",");
+    }
+  }
+
+  return string1
+}
+
+String extractAngles(String command) {
+  // Split command string by commas and colons
+  int servosToMove[NUM_SERVOS];
+  int angles[NUM_SERVOS];
+  int angleIndex = 0;
+
+  int start = 0;
+  int split = command.indexOf(':') + 1;
+  int end = command.indexOf(',');
+
+  while (end != -1) {
+    // Assign ids and angles to arrays
+    servosToMove[angleIndex] = command.substring(start, split).toInt();
+    angles[angleIndex] = command.substring(split, end).toInt();
+    angleIndex++;
+
+    start = end + 1;
+    split = command.indexOf(':', start) + 1;
+    end = command.indexOf(',', start);
+  }
+
+  // get the last pair
+  servosToMove[angleIndex] = command.substring(start, split).toInt();
+  angles[angleIndex] = command.substring(split).toInt();
+  angleIndex++;
+
+  // Send back the received angles
+  String string1 = "Received servos: ";
+
+  for (int i = 0; i < angleIndex; ++i) {
+    string1.concat(servos[i]);
+    if (i < angleIndex - 1) {
+      string1.concat(",");
+    }
+  }
+  return string1
+}
+
+test(extractCommand) {
+  assertEqual(extractAngles("2:69,3:80,4:93,8:40"), (String) "69,80,93,40");
+  assertEqual(extractServos("2:69,3:80,4:93,8:40"), (String) "2,3,4,8");
+  assertEqual(extractAngles("aaaaaaaa"), (String) "69,80,93,40");
+  assertEqual(extractServos("aaaaaaaa"), (String) "2,3,4,8");
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-  Serial.println("15 = ");
-  Serial.print(check(15, 5, 20));
-
-  Serial.println("5 = ");
-  Serial.print(check(15, 5, 20));
-
-  Serial.println("14 = ");
-  Serial.print(check(15, 5, 20));
 }
 
 void loop() {
-  if (1) {
-    String command = "2:69,3:80,4:93,8:40";
-
-    // Split command string by commas and colons
-    int servosToMove[NUM_SERVOS];
-    int angles[NUM_SERVOS];
-    int angleIndex = 0;
-
-    int start = 0;
-    int split = command.indexOf(':') + 1;
-    int end = command.indexOf(',');
-
-    while (end != -1) {
-      // Assign ids and angles to arrays
-      servosToMove[angleIndex] = command.substring(start, split).toInt();
-      angles[angleIndex] = command.substring(split, end).toInt();
-      angleIndex++;
-
-      start = end + 1;
-      split = command.indexOf(':', start) + 1;
-      end = command.indexOf(',', start);
-    }
-    
-    // get the last pair
-    servosToMove[angleIndex] = command.substring(start, split).toInt();
-    angles[angleIndex] = command.substring(split).toInt();
-    angleIndex++;
-
-    // for (int i = 0; i < angleIndex; ++i) {
-    //   int servoIndex = servosToMove[i];
-    //   servos[servoIndex].write(angles[i]);
-    // }
-
-    // Send back the received angles
-    Serial.print("Received angles: ");
-    for (int i = 0; i < angleIndex; ++i) {
-      Serial.print(angles[i]);
-      if (i < angleIndex - 1) {
-        Serial.print(",");
-      }
-    }
-    Serial.println();
-    Serial.print("Servos: ");
-    for (int i = 0; i < angleIndex; ++i) {
-      Serial.print(servosToMove[i]-2);
-      if (i < angleIndex - 1) {
-        Serial.print(",");
-      }
-    }
-    Serial.println();
-  }
-
-  delay(3000);
+  aunit::testrunner::run()
 }
